16d8d16d287a23ee04a35fd5b0f972b8ff65ca41
DeividVeloso <veloso.deivid@gmail.com>
2018-02-13 16:03:50 -0200

    add bearer token

--- spotifyWrapper/src/search.js
+++ spotifyWrapper/src/search.js
@@ -2,19 +2,19 @@
 
 import API_URL from './config';
 
-export const search = (query, type) => fetch(`${API_URL}search?q=${query}&type=${type}`, {
+export const search = (query, type, token = '') => fetch(`${API_URL}search?q=${query}&type=${type}`, {
   method: 'GET',
   headers: {
     Accept: 'application/json',
     'Content-Type': 'application/json',
-    Authorization: 'Bearer BQBsfzbwCwZ2wRNtzAtmbXxUN9HU-vrnZdkr6cWkZnv5DpDBsuQy2LeJcJTkmEryWAE_qajXnjGT4kHkPk_zhGwy3OGynOVA4iED-SBqkD7luVMmch2gPPCDWZWV5bNcTEsa87p7TUrgms9UjWTPB5UFV5gKuPC-NT6dB9O4swtUhRKbgm9_3EOodv0VA6tfqY34O2U37eLAMvQjlbspq3Tt4xufDsJggma2JV7mh0pqWoyRGYk8hFKXZoKWjBBgtATQKAKVuvYCR3LRJN72yl9gpU0',
+    Authorization: `Bearer ${token}`,
   },
 }).then(data => data.json());
 
-export const searchAlbum = query => search(query, 'albums');
+export const searchAlbum = (query, token) => search(query, 'albums', token);
 
-export const searchArtist = query => search(query, 'artist');
+export const searchArtist = (query, token) => search(query, 'artist', token);
 
-export const searchTracks = query => search(query, 'tracks');
+export const searchTracks = (query, token) => search(query, 'tracks', token);
 
-export const searchPlaylists = query => search(query, 'playlist');
+export const searchPlaylists = (query, token) => search(query, 'playlist', token);
