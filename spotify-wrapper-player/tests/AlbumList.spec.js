4a4d69a390413967b619973176c23789bab62387
DeividVeloso <veloso.deivid@gmail.com>
2018-03-28 19:32:21 -0300

    create javascript albumRender component, to generate markup of list albuns

--- spotify-wrapper-player/tests/AlbumList.spec.js
+++ spotify-wrapper-player/tests/AlbumList.spec.js
@@ -43,14 +43,102 @@ describe('Render AlbumList', () => {
   ];
   const markup = `
   <div class="list-item">
-    <img src="https://i.scdn.co/image/a13aadb368a956be48fe9089acb64333f0c0ca40" alt="" class="list-image">
-    <div class="list-description">
-      <p class="list-title">Make Yourself</p>
-      <p class="list-subtitle">Incubus</p>
-    </div>
-  </div>`
+  <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image">
+  <div class="list-description">
+    <p class="list-title">${data[0].name}</p>
+    <p class="list-subtitle">${data[0].artists[0].name}</p>
+  </div>
+  </div>`;
+
+  const data2 = [
+    {
+      album_type: 'album',
+      artists: [
+        {
+          name: 'Kings of leon',
+        },
+      ],
+      id: '3mZir9E3dueqyt15RIdxnf',
+      images: [
+        {
+          height: 640,
+          url:
+            'https://i.scdn.co/image/bd21447c72eb79021da04b8f949906c552bc505a',
+          width: 640,
+        },
+        {
+          height: 300,
+          url:
+            'https://i.scdn.co/image/87d268cf13849b42e1cf1fb82222c368318427c1',
+          width: 300,
+        },
+        {
+          height: 64,
+          url:
+            'https://i.scdn.co/image/740c43f9f3d13e98ac2699f83b1bce4b689eb119',
+          width: 64,
+        },
+      ],
+      name: 'Waste A Moment (Live)',
+      type: 'album',
+    },
+    {
+      album_type: 'album',
+      artists: [
+        {
+          name: 'Kings of leon',
+        },
+      ],
+      id: '3mZir9E3dueqyt15RIdxnf',
+      images: [
+        {
+          height: 640,
+          url:
+            'https://i.scdn.co/image/bd21447c72eb79021da04b8f949906c552bc505a',
+          width: 640,
+        },
+        {
+          height: 300,
+          url:
+            'https://i.scdn.co/image/87d268cf13849b42e1cf1fb82222c368318427c1',
+          width: 300,
+        },
+        {
+          height: 64,
+          url:
+            'https://i.scdn.co/image/740c43f9f3d13e98ac2699f83b1bce4b689eb119',
+          width: 64,
+        },
+      ],
+      name: 'Waste A Moment (Live)',
+      type: 'album',
+    },
+  ];
+
+  const markup2 = `
+  <div class="list-item">
+  <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image">
+  <div class="list-description">
+    <p class="list-title">${data[0].name}</p>
+    <p class="list-subtitle">${data[0].artists[0].name}</p>
+  </div>
+  </div>
+  <div class="list-item">
+  <img src="${data[0].images[2].url}" alt="${data[0].name}" class="list-image">
+  <div class="list-description">
+    <p class="list-title">${data[0].name}</p>
+    <p class="list-subtitle">${data[0].artists[0].name}</p>
+  </div>
+  </div>`;
 
   it('should create and append the markup given a correct data', () => {
-
+    const element = document.createElement('div');
+    renderAlbums(data, element);
+    expect(element.innerHTML).to.be.eql(markup);
+  });
+  it('should create and append when more than 1 album', () => {
+    const element2 = document.createElement('div');
+    renderAlbums(data2, element2);
+    expect(element2.innerHTML).to.be.eql(markup2);
   });
 });
--- spotifyWrapper/examples/search.js
+++ spotifyWrapper/examples/search.js
@@ -3,10 +3,10 @@ import SpotifyWrapper from '../src';
 global.fetch = require('node-fetch');
 
 const token =
-  'BQAqWtknzxSKNaUp0k7K0oyx9K7_2gw-bZLwNvvr0qTT_XdN8Wbn1lDtaDAV1TvgnXLkfYJIq4flypnygshxUI_SLXxXHRQBd_-92B7Xa1Q6q9y_CsX72NJAnukj1lEXpVd7lR6ndDEr8q8Yki3h0kLMScV7sxhlZD7SoXZTWFWgwPnVj3rhlGtF1d_K86X4vzjglY-KSyv27uxQriPZjS6IzEqROKxBNRUhjGQTtxwnGsP3S-s8bUYRpGSIobCDAaHf8hp1YyvL4cZTyhLIcixa7XY';
+  'BQCr52IwMhb2PfbbJBDVZSymQ9aM_dv67ENKmem8AJbYrsDsEW0Ja8QHREqyGNsqNfDOW0zGJGo2xt7FIOyC5ha7jgemRW5TktvAHUoTe-BDONaG0tvdz5ngfY6dlAzgQw19uRTAVdqyqGjZEsJWZBPky6daqNhiWcZ5WnGRW06gO-rjMaQu6djT3xuiuCsVRS-MYJQUkDPwIpuvgJZU8fkllJLZxhvYlA71dJ1l3SEeQYeFtXUoK_mHKMhOLsfMTIXCcx3Lzna162AcuBEt8cvTBRo';
 const spotify = new SpotifyWrapper({
   token
 });
 
 const albums = spotify.search.albums('Kings of leon');
-albums.then(data => data.albums.items.map(item => console.log(item.name)));
+albums.then(data => data.albums.items.map(item => console.log(item)));
