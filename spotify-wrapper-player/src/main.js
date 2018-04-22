50f42c21abe52ead00a5947c78905a2fc8f643df
Deivid Veloso <veloso.deivid@gmail.com>
2018-04-22 13:23:56 -0300

    finish player spotify

--- spotify-wrapper-player/src/main.js
+++ spotify-wrapper-player/src/main.js
@@ -1,22 +1,10 @@
-import spotify from './Spotify';
-import renderAlbums from './AlbumList'
-import renderAlbumInfo from './AlbumInfo'
-import renderAlbumTracks from './AlbumTracks'
+import searchEnterTrigger from './SearchTrigger'
+import selectAlbumTrigger from './SearchAlbumTrigger'
+import playlistTrigger from './PlaylistTrigger'
 
+searchEnterTrigger();
+selectAlbumTrigger();
+playlistTrigger();
 
-const albumList = document.getElementById('album-list');
-const albums = spotify.search.albums('Kings of leon');
-albums.then(data => {
-    return renderAlbums(data.albums.items, albumList)
-})
-
-//Pegando o album Info
-const album = spotify.album.getAlbum('3DxR2xJJfAGJlbArx7wAgm');
-const albumInfo = document.getElementById('album-info');
-
-const albumTracks = document.getElementById('album-musics-tracks');
-album
-.then(data => renderAlbumInfo(data, albumInfo)) // Pegando album info
-.then(data => renderAlbumTracks(data.tracks.items, albumTracks)) //Pegando album tracks
 
 
